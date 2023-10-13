6dfc383fe4bd2e153faecf6a2e7873245533d61f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2023-10-13 09:23:04 +0200

    import export

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-import { renderTitles, clearTitleList } from "./js/render/title.js";
-
-// the url to the API
-const url = "https://api.noroff.dev/api/v1/gamehub";
-
-// function calling the api to fetch an array of objects and returning the data
-async function getTitles() {
-  // fetch
-  const response = await fetch(url);
-
-  const data = await response.json();
-
-  const titles = data;
-
-  return titles;
-}
+import { getTitles } from "./js/fetch/fetch.js";
+import { renderTitles } from "./js/render/title.js";
 
 // data from API response saved to the variable as an array of objects
-const titles = await getTitles();
+let titles = await getTitles();
 
 //
 //
@@ -24,5 +10,5 @@ const titles = await getTitles();
 
 const parent = document.querySelector(".new-releases-container");
 
-clearTitleList(parent);
 renderTitles(titles, parent);
+console.log(titles);
