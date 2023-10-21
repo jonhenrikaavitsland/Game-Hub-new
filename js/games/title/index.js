92a617208aee76eacf7fb0360418195fc585e0bf
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2023-10-21 08:01:32 +0200

    testing stuff

--- js/games/title/index.js
+++ js/games/title/index.js
@@ -1,4 +1,4 @@
-import { createHTML } from "../../render/renderTitle.js";
+import { createHTML } from "./../../render/renderTitle.js";
 
 // location for rendering html
 export const titleContainer = document.querySelector(".title-wrap");
@@ -7,24 +7,24 @@ export const title = document.querySelector("title");
 // getting querystring from parent page
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const id = params.get("id");
+export const id = params.get("id");
 
 // url for fetching title details
-const url = "https://api.noroff.dev/api/v1/gamehub/" + id;
+export const url = "https://api.noroff.dev/api/v1/gamehub/" + id;
 
 // fetching title for title page and rendering the html
-async function fetchTitle() {
+export async function fetchTitle() {
   try {
     const response = await fetch(url);
     const details = await response.json();
 
-    console.log(details);
-
     createHTML(details);
+    return details;
   } catch (error) {
     console.log(error);
     titleContainer.innerHTML = message("error", error);
   }
 }
 
-fetchTitle();
+const game = await fetchTitle();
+console.log(game);
